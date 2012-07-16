091933f81aee084096815bcf2b86e6c8823db3b8
dakbill <dakbill@yahoo.com>
2012-07-16 11:30:44 +0000

    am on errtin

--- ideabank/ideabank/settings.py
+++ ideabank/ideabank/settings.py
@@ -12,14 +12,9 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ideabank.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': 
+        dj_database_url.config(default="sqlite:/ideapage_dev.db")
+    
 }
 
 # Local time zone for this installation. Choices can be found here:
